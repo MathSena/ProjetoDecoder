2265f1bd731ce7b809f63251a94f1574c099cd6a
MathSena <MathSena07@hotmail.com>
2023-08-16 22:52:09 +0100

    TASK-20 feat: Map Entities, create Repositories and Services for Multiple Entities

---
+++ course/src/main/java/com/ead/course/enums/CourseLevel.java
@@ -0,0 +1,5 @@
+package com.ead.course.enums;
+
+public enum CourseLevel {
+    BEGINER, INTERMEDIATE, ADVANCED
+}
---
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ead.course.services.impl;
+
+
+import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.services.ModuleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModuleServiceImpl implements ModuleService {
+
+    private final ModuleRepository moduleRepository;
+
+    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+        this.moduleRepository = moduleRepository;
+    }
+}
