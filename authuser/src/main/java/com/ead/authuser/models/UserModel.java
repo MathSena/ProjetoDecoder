d8cb28c8239b382ee1fc58259a768b34eca00199
MathSena <MathSena07@hotmail.com>
2023-08-22 22:39:43 +0100

    TASK-28 - Best Practices for Dates in APIs using Standard ISO 8601 UTC

--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -47,10 +47,10 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @Column
     private String imageUrl;
     @Column(nullable=false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime createdDate;
     @Column(nullable=false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime lastUpdateDate;
 
 
