ca01cf1f60cd1381a2fd7eed36ef57a44997409c
MathSena <MathSena07@hotmail.com>
2024-03-19 22:17:55 +0000

    Refactor ResolverConfig to use proper indentation and formatting. Fix argumentResolver declaration and add missing imports.

--- authuser/src/main/java/com/ead/authuser/publishers/UserEventPublisher.java
+++ authuser/src/main/java/com/ead/authuser/publishers/UserEventPublisher.java
@@ -2,7 +2,7 @@ package com.ead.authuser.publishers;
 
 import com.ead.authuser.dtos.UserEventDto;
 import com.ead.authuser.enums.ActionType;
-import org.springframework.  uamqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
