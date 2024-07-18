384004edfd1730cb4e1a30eea307651f42799a81
MathSena <MathSena07@hotmail.com>
2024-07-18 19:10:30 +0100

    Added resilience4j retry configuration and updated RestTemplateConfig to set connect and read timeout to 5000 milliseconds. Also, added dependencies for circuit breaker and actuator in pom.xml. Added retry annotation to CourseClient method.

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,6 +33,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    @Retry(name = "retryInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -47,9 +49,10 @@ public class CourseClient {
                     .getContent();
             log.debug("Response Number of Elements: {} ", searchResult.size());
         } catch (HttpStatusCodeException e) {
-            log.error("Error request /courses {} ", e);
+            log.error("Error request /courses { } ", e);
         }
         log.info("Ending request /courses userId {} ", userId);
+        assert result != null;
         return result.getBody();
     }
 
