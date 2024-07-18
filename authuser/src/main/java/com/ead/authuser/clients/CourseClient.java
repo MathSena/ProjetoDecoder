e1044abcd6d8159a5d834b310456e0d1321df27d
MathSena <MathSena07@hotmail.com>
2024-07-18 19:29:59 +0100

    Commit message: Add resilience4j circuit breaker annotation to CourseClient getAllCoursesByUser method

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -33,7 +35,8 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
-    @Retry(name = "retryInstance")
+    @Retry(name = "retryInstance", fallbackMethod = "retryFallback")
+    @CircuitBreaker(name = "circuitBreakerInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -54,6 +57,12 @@ public class CourseClient {
         log.info("Ending request /courses userId {} ", userId);
         assert result != null;
         return result.getBody();
+    }
+
+    public List<CourseDto> retryFallback(UUID userId, Pageable pageable, Throwable throwable) {
+        log.error("Error request /courses { } ", throwable);
+        List<CourseDto> searchResult = new ArrayList<>();
+        return searchResult;
     }
 
 }
